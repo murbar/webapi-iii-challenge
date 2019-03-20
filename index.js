f7a5a57f6bd4855b76e3f6e5411bbca4eb9f50d3
Joel Bartlett <joel@murbar.com>
2019-03-20 17:00:00 -0400

    Tidy up

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const server = express();
 const cors = require('cors');
+
 const posts = require('./routes/posts');
 const users = require('./routes/users');
 
-const port = 4000;
-
 server.use(express.json());
 server.use(cors());
+
 server.use('/api/posts', posts);
 server.use('/api/users', users);
+
+const port = 4000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
