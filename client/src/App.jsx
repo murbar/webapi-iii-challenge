af6ffd116f6050894dfb97d45d949774dd5bda11
Joel Bartlett <joel@murbar.com>
2019-03-20 18:31:24 -0400

    Render components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Router } from '@reach/router';
 import './App.css';
+import useResource from './hooks/useResource';
+import UsersList from './components/UsersList';
+import UserPosts from './components/UserPosts';
 
 const App = () => {
+  const userStore = useResource('http://localhost:4000/api/users');
+
   return (
     <main>
-      <h1>Blogs</h1>
+      <Router>
+        <UsersList path="/" userStore={userStore} />
+        <UserPosts path="/:userId" userStore={userStore} />
+      </Router>
     </main>
   );
 };
